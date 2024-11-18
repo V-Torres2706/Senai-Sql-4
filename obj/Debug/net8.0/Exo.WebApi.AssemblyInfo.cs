fbebdfa5d317cebd371f35fdca3aa2eb37c4672c
Victor <victortmonte@outlook.com>
2024-11-18 20:04:41 -0300

    Continuação

--- obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611fb2e78c34061fae1f4338a54b8a7c79231675")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
